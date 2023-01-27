e9b6f9f0a970fc149031870c6565ac0943b32d2a
Salar Hafezi <salar.hfz.work@gmail.com>
2023-01-27 13:47:28 +0330

    add hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar'
 import './App.scss'
+import Hero from './components/Hero'
 
 function App() {
   return (
     <div className="container">
       <Navbar />
+      <Hero />
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+function Hero() {
+    return (
+        <section className="hero">
+            <div className="hero__img-container">
+                <img alt="Airbnb experiences shots" src="photo-grid.png" className="hero__img-container__img" />
+            </div>
+            <h1 className="hero__title">Online Experiences</h1>
+            <p className="hero__desc">
+                Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+            </p>
+        </section>
+    )
+}
+
+export default Hero;
